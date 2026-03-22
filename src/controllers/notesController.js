556903c4f47e36cdbaa5d4cc7464999b246408a4
Alina Slipenka <a.slipenka@gmail.com>
2026-03-22 22:09:17 +0200

    add hw2.2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
@@ -12,5 +12,39 @@ export const getNoteById = async (req, res) => {
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId }, // Шукаємо по id
+    req.body,
+    { returnDocument: 'after' }, // повертаємо оновлений документ
+  );
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
   res.status(200).json(note);
 };
