a3c2463f3456a08f1a6e379d7fa5ccbbac57a549
Alina Slipenka <a.slipenka@gmail.com>
2026-04-04 13:59:59 +0300

    add hm

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -16,6 +18,10 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
+// підключення валідації
+app.use(authRoutes);
 
 // підключаємо групу маршрутів нотаток
 app.use(notesRoutes);
